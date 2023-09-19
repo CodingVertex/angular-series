ff2d592c8ceb9dff687c811ed06a67eadefc156b
CodingVertex <143869252+CodingVertex@users.noreply.github.com>
2023-09-19 13:07:17 +0530

    first component commited

---
+++ src/app/first-component1/first-component1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component1',
+  templateUrl: './first-component1.component.html',
+  styleUrls: ['./first-component1.component.scss']
+})
+export class FirstComponent1Component {
+
+}
