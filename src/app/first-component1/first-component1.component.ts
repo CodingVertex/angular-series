2f75034f79a8b4577ed7e8d817972e80ef90d326
CodingVertex <143869252+CodingVertex@users.noreply.github.com>
2023-09-19 14:08:31 +0530

    commiting the 1st video project modification

--- src/app/first-component1/first-component1.component.ts
+++ src/app/first-component1/first-component1.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-component1',
-  templateUrl: './first-component1.component.html',
-  styleUrls: ['./first-component1.component.scss']
+  template: `
+    <div class="container">
+    <h3>This is first component</h3>
+    </div>
+  `,
+  styles: [`
+    .container {
+      border: 2px solid;
+      padding: 10px 20px;
+      width: 200px;
+    }
+  `]
 })
 export class FirstComponent1Component {
 
